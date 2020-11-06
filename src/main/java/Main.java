045781c99357f70d41a7594078e3c0a8366a4f85
06028 <06028@farnborough.ac.uk>
2020-11-06 14:48:14 +0000

    yes

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String message = "Weather Update!!";
+        System.out.println(message);
+        System.out.println(message.length());
+    }
+
+}
